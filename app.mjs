5b7cd7844f4aa570f9122761293ac92f6e2a4910
Paul Miller <1manaramy72@gmail.com>
2026-03-13 12:43:45 +0100

    feat: migrate from GPS job to TrackCar API, removing obsolete GPS integration

--- app.mjs
+++ app.mjs
@@ -10,7 +10,7 @@ import {
 } from './scr/telegram/middleware/middleware.commands.mjs';
 import { onInline } from './scr/telegram/middleware/middleware.inline-handler.mjs';
 import { runBitrixJob } from './scr/bitrix/bootstrap.mjs';
-import { runGpsJob } from './scr/gps/bootstrap.mjs';
+
 import { carPickResponse } from './scr/telegram/middleware/middleware.car-pick-response.mjs';
 import { enterCarVisScene } from './scr/telegram/middleware/middleware.enter-car-vis-scene.mjs';
 import { sceneCarVis } from './scr/telegram/scenes/scene.car.vis.mjs';
@@ -30,7 +30,6 @@ const app = express();
 
 (async () => {
   runBitrixJob();
-  runGpsJob();
 
   bot.start(onStart);
 
